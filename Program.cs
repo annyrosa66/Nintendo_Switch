39caf9d439b875c22f82af53e092e13c241c3f06
annyrosa66 <chocolatecc23@gmail.com>
2024-03-10 21:48:38 +0100

    Eso e 11

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Operacion suma y resta");
+Console.WriteLine();
+
+Console.WriteLine("Elija una opcion (suma = s | Resta = r)");
+char operacion = Convert.ToChar(Console.ReadLine());
+
+switch (operacion)
+{
+    case 's':
+        Console.WriteLine("Ha seleccionado la opcion suma");
+        break;
+    case 'r':
+        Console.WriteLine("Ha seleccionado la opcion resta");
+        break;
+    default:
+        Console.WriteLine("Ha seleccionado la opcion incorrecta");
+        break;
+
+       
+}
+Console.WriteLine("Programa terminado");
